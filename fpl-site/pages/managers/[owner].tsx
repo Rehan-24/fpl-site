b567c5876a5fd8871a9a1537c0da33d35eed6a34
Rehan-24 <rikhan6855@gmail.com>
2025-08-09 00:30:24 -0700

    updating manager info 5/n

--- fpl-site/pages/managers/[owner].tsx
+++ fpl-site/pages/managers/[owner].tsx
@@ -129,7 +129,7 @@ export default function ManagerBio() {
           {/* text block (indented to clear image) */}
           <div className="relative p-5 sm:p-6 ml-28 sm:ml-40 md:ml-48">
             <div className="text-base sm:text-lg opacity-90">{first}</div>
-            <div className="text-3xl sm:text-4xl font-extrabold -mt-1 leading-tight">
+            <div className="text-3xl sm:text-3xl font-extrabold -mt-1 leading-tight">
               {last || first}
             </div>
 
